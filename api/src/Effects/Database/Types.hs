027965c06c885ae286ab597f619cf813e97ea961
Jérémy Jousse <jeremy.jousse@decathlon.com>
2020-07-09 18:18:01 +0200

    feat(brand): add the brand database type

---
+++ api/src/Effects/Database/Types.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Effects.Database.Types where
+
+import Data.Aeson
+import Data.Text
+import Database.Persist ()
+import Database.Persist.Sql (Migration, addMigration)
+import Database.Persist.TH
+
+-- import qualified Model.Brand as Brand
+
+share
+  [mkPersist sqlSettings, mkMigrate "migrateAll"]
+  [persistLowerCase|
+  Brand
+      name Text
+      url Text
+      deriving Show
+  |]
--- api/src/Lib.hs
+++ api/src/Lib.hs
@@ -2,5 +2,19 @@ module Lib
     ( someFunc
     ) where
 
+import Control.Monad.IO.Class  (liftIO)
+import Control.Monad.Logger    (runStderrLoggingT)
+import Effects.Database.Types
+import Database.Persist
+import Database.Persist.Postgresql
+
+connStr = "host=localhost dbname=test user=test password=test port=5432"
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = runStderrLoggingT $ withPostgresqlPool connStr 10 $ \pool -> liftIO $ do
+    flip runSqlPersistMPool pool $ do
+        runMigration migrateAll
+
+        brandId <- insert $ Brand "Decathlon" "https://www.decathlon.fr"
+
+        pure ()
